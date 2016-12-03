ab3200a03015ee38c7775260ce5d549508f78095
Gus Henry <alhenry@andrew.cmu.edu>
2016-12-02 21:07:50 -0500

    Initial Commit

---
+++ TulubeTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TulubeTV
+//
+//  Created by Gus Henry on 12/2/16.
+//  Copyright © 2016 Gus Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
